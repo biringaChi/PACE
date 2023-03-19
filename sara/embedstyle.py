143ad9cf56e8d3a0549f3d90007f07518b98ff64
Biringa Chidera <biringachidera@gmail.com>
2023-03-19 19:05:07 -0400

    file update

--- sara/embedstyle.py
+++ sara/embedstyle.py
@@ -18,24 +18,24 @@ class EmbedStyle(HandleRepo):
 		vectors = Word2Vec(sentences = data, vector_size = 32).wv
 		vectors.save(pathlib.Path.cwd().parents[0]/"embeddings"/vec_name)
 	
-	def _vectors_match(vectors, observations):
+	def _vector_match(vectors, observations):
 		vec_dict = {}
 		model = KeyedVectors.load(vectors, mmap = "r")
 		for key in model.key_to_index.keys():
 			vec_dict[key] = model[key]
-		out = []
+		embeddings = []
 		for embeds in observations:
 			temp = []
 			for embed in embeds:
 				if embed in vec_dict:
 					temp.append(vec_dict[embed])
-			out.append(temp)
-		return out
+			embeddings.append(temp)
+		return embeddings
 		
 	def __call__(self):
 		source_files = self.get_project(self.project)
 		trees, _ = self.get_trees(source_files)
-		out = []
+		features = []
 		for tree in trees:
-			out.append([node.__class__.__name__ for _, node in tree if node.__class__.__name__ in self.features])
-		return out
+			features.append([node.__class__.__name__ for _, node in tree if node.__class__.__name__ in self.features])
+		return features
