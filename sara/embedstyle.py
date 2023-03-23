32cfbeb182d392855f90f543686ba0794ae509bd
Biringa Chidera <biringachidera@gmail.com>
2023-03-23 18:43:15 -0400

    code update

--- sara/embedstyle.py
+++ sara/embedstyle.py
@@ -12,18 +12,19 @@ class EmbedStyle(HandleRepo):
 	def __init__(self, project = None) -> None:
 		super().__init__()
 		self.cfg = Config()
+		self.project = project
 		self.seqlen = self.cfg.max_seqlen
 		self.ft = self.cfg.feature_types
 		self.features = self.ft["statements"] + self.ft["expressions"] + self.ft["controls"] + self.ft["invocations"] + self.ft["declarations"]
 	
-	def _node_selection(self, tree):
+	def node_selection(self, tree) -> typing.List:
 		return [node.__class__.__name__ for _, node in tree if node.__class__.__name__ in self.features]
 
-	def _model(data, vec_id):
+	def model(data, vec_id):
 		vectors = Word2Vec(sentences = data, vector_size = 32).wv
 		vectors.save(pathlib.Path.cwd().parents[0]/"embeddings"/vec_id)
 	
-	def _vector_match(vectors, observations):
+	def vector_match(vectors, observations):  
 		vec_dict = {}
 		model = KeyedVectors.load(vectors, mmap = "r")
 		for key in model.key_to_index.keys():
@@ -37,10 +38,10 @@ class EmbedStyle(HandleRepo):
 			embeddings.append(temp)
 		return embeddings
 	
-	def _truncate(self, embeddings):
+	def truncate(self, embeddings):
 		return [vector[:self.seqlen] if self.__len__(vector) > self.seqlen else vector for vector in embeddings] 
 		
-	def _zeropad(self, embeddings):
+	def zeropad(self, embeddings):
 		out  = []		
 		for embedding in embeddings:
 			if len(embedding) < self.seqlen:
@@ -53,19 +54,18 @@ class EmbedStyle(HandleRepo):
 				else: temp.append(vector)
 			out.append(temp)
 		return out
-
-	def _store_vectors(self, vector):
-		pass
-
-	def _load_vectors(self, vector):
-		pass
-		
+	
+	def flatten(self, embeddings):
+		out = []
+		for embedding in embeddings:
+			if not embedding:
+				out.append(embedding)
+			else:
+				flatten_list = np.concatenate(embedding).ravel().tolist()
+				out.append(flatten_list) 
+		return out
+			
 	def __call__(self):
-		source_files = self.get_project(self.project)
-		trees, _ = self.get_trees(source_files)
-		features = [self._node_selection(tree) for tree in trees]
-		observations = self._model(features, "vectors.kv")
-		embeddings = self._vector_match(observations)
-		embeddings = self._truncate(embeddings)
-		embeddings = self._zeropad(embeddings)
-		return embeddings
+		trees, _ = self.get_trees(self.get_project(self.project))
+		features = [self.node_selection(tree) for tree in trees]
+		return self.zeropad(self.truncate(self.vector_match(self.model(features, "vectors.kv"))))
