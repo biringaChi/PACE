0ed55068a3fd14e204fbfa447546ce3eee1ac6be
Biringa Chidera <biringachidera@gmail.com>
2023-05-17 13:18:09 -0400

    non-functional update 0.8

---
+++ pace/freqstyle.py
@@ -0,0 +1,58 @@
+import math
+import typing
+import numpy as np
+from config import Config
+from repohandle import HandleRepo
+
+class FreqStyle(HandleRepo):
+	"""
+	Numerical Statistic Representation of Extracted Code Stylometry Features
+	"""
+	def __init__(self, project = None) -> None:
+		super().__init__()
+		self.config = Config()
+		self.project = project
+		self.feature_types = self.config.feature_types
+		self.stmt_features = self.feature_types["statements"]
+		self.expr_features = self.feature_types["expressions"]
+		self.ctrl_features = self.feature_types["controls"]
+		self.invctn_features = self.feature_types["invocations"]
+		self.dclrtn_feautes = self.feature_types["declarations"]
+	
+	def _FE(self, source_file, feature, numstat):
+		with np.errstate(divide = "ignore"):
+			datapoint = -np.log10(feature / self.__len__(source_file))
+			numstat.append(0.0) if math.isinf(datapoint) else numstat.append(datapoint)
+	
+	def _node_selection(self, tree, features):
+		return self.__len__([True for _, node in tree if node.__class__.__name__ in features])
+
+	def __call__(self) -> typing.Dict:
+		statements, expressions, controls, invocations, declarations = ([] for _ in range(5))
+		source_files = self.get_project(self.project)
+		trees, _ = self.get_trees(source_files)
+		for tree in trees:
+			statements.append(self._node_selection(tree, self.stmt_features))
+			expressions.append(self._node_selection(tree, self.expr_features))
+			controls.append(self._node_selection(tree, self.ctrl_features))
+			invocations.append(self._node_selection(tree, self.invctn_features))
+			declarations.append(self._node_selection(tree, self.dclrtn_feautes))
+		numstat_stmts, numstat_expr, numstat_ctrls, numstat_invctn, numstat_dclrtn  = ([] for _ in range(5)) 
+		for source_file, stmt_feature, expr_feature, ctrl_feature, invctn_feature, dclrtn_feature in zip(
+			source_files, 
+			statements, 
+			expressions, 
+			controls,
+			invocations, 
+			declarations
+			):
+			self._FE(source_file, stmt_feature, numstat_stmts)
+			self._FE(source_file, expr_feature, numstat_expr)
+			self._FE(source_file, ctrl_feature, numstat_ctrls)
+			self._FE(source_file, invctn_feature, numstat_invctn)
+			self._FE(source_file, dclrtn_feature, numstat_dclrtn)
+			
+		return {
+			"syntactic" : (numstat_stmts, numstat_expr, numstat_ctrls),
+			"lexical" : (numstat_invctn, numstat_dclrtn)
+		}
---
+++ pace/repohandle.py
@@ -0,0 +1,67 @@
+import os
+import re
+import pathlib
+import javalang
+import pickle
+import pandas as pd
+from config import Config
+from javalang.tree import CompilationUnit
+from javalang.parser import JavaSyntaxError
+from typing import Tuple, Dict, List, Sequence, Set, Text, Union
+
+class HandleRepo:
+	def __init__(self) -> None:
+		self.config = Config()
+
+	def __len__(self, arg: Union[Sequence, Text, Dict, Set]) -> int:
+		if (isinstance(arg, (int, float, bool))):
+			raise TypeError("Invalid argument. Only text, sequence, mapping and set are accepted")
+		else:
+			return len(arg)
+
+	def pickle(self, data, file_name):
+		with open(file_name, "wb") as file:
+			pickle.dump(data, file)
+	
+	def unpickle(self, data):
+		with open(data, "rb") as file:
+			loaded = pickle.load(file)
+		return loaded
+
+	def get_trees(self, source_files) -> CompilationUnit:
+		trees = []
+		uncompiled_sourcecode = {}
+		for idx, sourcecode in enumerate(source_files):
+			try:
+				trees.append(javalang.parse.parse(sourcecode))
+			except JavaSyntaxError as e:
+				uncompiled_sourcecode[idx] = sourcecode
+				trees.append(None)
+		return trees, uncompiled_sourcecode
+
+	def get_project(self, project) -> Tuple[List[str], List[str]]:
+		source_files, source_file_names = [], []
+		for root, _, files in os.walk(project):
+			for file in files:
+				if file.endswith(".java"):
+					temp = os.path.join(root, file)
+					source_file_names.append(pathlib.Path(temp).stem)
+					try:
+						with open(temp, "r") as source_file:
+							source_files.append(source_file.read())
+					except OSError as e:
+						raise e
+		return source_files, source_file_names
+
+	def get_runtime(self, runtime_path: Union[str, Tuple[str, str]]) -> List[float]:
+		runtime: pd.DataFrame = pd.read_csv(runtime_path)
+		file_id = runtime_path.split("/")[-1].lower()
+		if re.search(self.config.dubbo_h2, file_id):
+			file_name = runtime[self.config.runtime_c].apply(lambda x:x.split(";")[0]).apply(lambda x:x.split(".")[-1])
+			runtime_ms = runtime[self.config.runtime_c].apply(lambda x:x.split(";")[1])
+			return pd.DataFrame({self.config.file_name:file_name, self.config.runtime_ms:runtime_ms})
+		elif re.search(self.config.rdf4j, file_id):
+			return pd.DataFrame({self.config.file_name:runtime[self.config.test_case], self.config.runtime_ms:runtime[self.config.runtime_c_ms]})
+		else:
+			runtime: pd.DataFrame = pd.concat([pd.read_csv(runtime_path[0]), pd.read_csv(runtime_path[1])])
+			return pd.DataFrame({self.config.file_name: runtime[self.config.test_file], self.config.runtime_ms:runtime[self.config.runtime_c_ms]})
